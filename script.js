b0cc8c0a1362f98f7250cf6fcd5885d0e212ec7f
Jose Rangel <josemiguelrangel075@gmail.com>
2024-05-22 18:32:16 -0600

    key events deleted

--- script.js
+++ script.js
@@ -4,82 +4,12 @@ let btns = document.querySelectorAll('button');
 let operation = '';
 let clearScreen;
 
-// Input keyboard functions
-screen.addEventListener('keydown', (e) => {
-  // Clear input after an invalid input inserted
-  if (clearScreen) {
-    screen.value = '';
-    operation = '';
-  }
-
-  clearScreen = false;
-  switch (e.key) {
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    case '0':
-      operation += e.key;
-      break;
-
-    case '%':
-      operation += '/100*';
-      break;
-    case '+':
-      operation += '+';
-      break;
-    case '-':
-      operation += '-';
-      break;
-    case '*':
-      operation += '*';
-      break;
-    case '/':
-      operation += '/';
-      break;
-    case '(':
-      operation += '(';
-      break;
-    case ')':
-      operation += ')';
-      break;
-    case 'Backspace': {
-      operation = operation.slice(0, -1);
-      break;
-    }
-    case 'Enter': {
-      try {
-        operation = eval(operation).toString();
-        // Shrink down result if number is greater than 15 digits
-        if (operation.length > 15) {
-          operation = parseInt(operation).toExponential(10).toString();
-        }
-        screen.value = numberWithCommas(operation);
-      } catch (err) {
-        screen.value = 'error';
-      }
-      break;
-    }
-    case 'Shift':
-      break;
-    default:
-      e.preventDefault();
-      screen.value = '(invalid input)';
-      clearScreen = true;
-  }
-});
-
 // Button click functions
 for (button of btns) {
   // Numbers
   if (button.classList.contains('number')) {
     button.addEventListener('click', (e) => {
-      screen.value += e.target.value;
+      screen.innerText += e.target.value;
       operation += e.target.value;
     });
   }
@@ -88,17 +18,17 @@ for (button of btns) {
     button.addEventListener('click', (e) => {
       // Percentage
       if (e.target.value === '%') {
-        screen.value += e.target.value;
+        screen.innerText += e.target.value;
         operation += '/100*';
       }
       // Power
       else if (e.target.value === '**') {
-        screen.value += '^';
+        screen.innerText += '^';
         operation += e.target.value;
       }
       // Other functions
       else {
-        screen.value += e.target.innerText;
+        screen.innerText += e.target.innerText;
         operation += e.target.value;
       }
     });
@@ -106,15 +36,17 @@ for (button of btns) {
   // Clear screen
   else if (button.value === 'CE') {
     button.addEventListener('click', () => {
-      screen.value = '';
+      screen.innerText = '';
       operation = '';
     });
   }
   // Delete character
   else if (button.value === 'delete') {
     button.addEventListener('click', () => {
-      screen.value = screen.value.slice(0, -1);
-      operation = operation.slice(0, -1);
+      if (screen.innerText.length > 0) {
+        screen.innerText = screen.innerText.slice(0, -1);
+        operation = operation.slice(0, -1);
+      } else screen.innerText = '';
     });
   }
   // Equal
@@ -126,15 +58,15 @@ for (button of btns) {
         if (operation.length > 15) {
           operation = parseInt(operation).toExponential(10).toString();
         }
-        screen.value = numberWithCommas(operation);
+        screen.innerText = numberWithCommas(operation);
       } catch (err) {
-        screen.value = 'error';
+        screen.innerText = 'error';
       }
     });
   }
 }
 
-// Formmat number with commas
+// Format number with commas
 function numberWithCommas(x) {
   let parts = x.toString().split('.');
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
